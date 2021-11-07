cc0f6049ee107b4312fb9b21754ac568f3c47eee
Vignesh Jeyaraman <vignesh.jeyaraman@outlook.com>
2021-11-07 17:41:44 +0530

    added api authentication in todo apis

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,3 +5,8 @@ class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
         exclude = ("created_at", "updated_at")
+
+class CreateTodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        exclude = ("created_at", "updated_at", "created_by")
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,19 @@
 from rest_framework.viewsets import ModelViewSet   
 from todo.models import Todo
-from todo.serializers import TodoSerializer
+from todo.serializers import TodoSerializer, CreateTodoSerializer
+from rest_framework.permissions import IsAuthenticated
 class TodoViewSet(ModelViewSet):
     """
     A viewset for viewing and editing user instances.
     """
-    serializer_class = TodoSerializer
-    queryset = Todo.objects.all()
+    permission_class = (IsAuthenticated,)
+    def get_queryset(self):
+        return Todo.objects.filter(created_by=self.request.user).all()
+    
+    def get_serializer_class(self):
+        if self.action == "create" or self.action == "update":
+            return CreateTodoSerializer
+        return TodoSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
