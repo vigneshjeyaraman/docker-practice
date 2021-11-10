caeafaf8194525ecc6dcf026b3f47a5796075958
Vignesh Jeyaraman <vignesh.jeyaraman@outlook.com>
2021-11-10 19:11:12 +0530

    serialized created_by in Todo serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from todo.models import Todo
+from users.serializers import UserSerializer
 
 class TodoSerializer(serializers.ModelSerializer):
+    created_by = UserSerializer()
     class Meta:
         model = Todo
         exclude = ("created_at", "updated_at")
